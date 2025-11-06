2109b6f7088e6790820c90246501f5fe222fcfd4
Isaac <isaacantonioramosperez@alumno.ieselrincon.es>
2025-11-06 17:02:57 +0100

    preparando variables agrosos

--- agroSoSFrontend/src/App.jsx
+++ agroSoSFrontend/src/App.jsx
@@ -1,8 +1,10 @@
 import {BrowserRouter, Route, Routes} from 'react-router-dom'
-// import Home from './components/Home';
 import './App.css'
+import Home from './views/Home';
 
 function App() {
+
+
   return (
     <>
     <BrowserRouter>
---
+++ agroSoSFrontend/src/views/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import fakeData from '../data/sensor'
+
+function Home() {
+  console.log(fakeData)
+
+  return (
+    <div>Home</div>
+  )
+}
+
+export default Home
