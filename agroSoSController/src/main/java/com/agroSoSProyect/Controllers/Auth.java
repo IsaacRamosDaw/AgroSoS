13fd9b86965681a411147dc20a2174348671ca3c
gabye80 <gabye80@gmail.com>
2025-11-06 16:39:34 +0000

    User backend

---
+++ agroSoSController/src/main/java/com/agroSoSProyect/Controllers/Auth.java
@@ -0,0 +1,4 @@
+package com.agroSoSProyect.Controllers;
+
+public class Auth {
+}
---
+++ agroSoSController/src/main/java/com/agroSoSProyect/Repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.agroSoSProyect.Repository;
+// Qué hace el jpaRepository
+import com.agroSoSProyect.Models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+// Qué hace el stereotype Repository
+import org.springframework.stereotype.Repository;
+
+// Qué hace el @Repository
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
