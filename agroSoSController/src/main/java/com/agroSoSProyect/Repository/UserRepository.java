c6a1a219f118f897714d542c4f955bc50f481728
isaac <isaacantonioramosperez@alumno.ieselrincon.es>
2025-11-20 02:15:59 +0100

    Auth y sensors hechos v3

--- agroSoSController/src/main/java/com/agroSoSProyect/Repository/UserRepository.java
+++ agroSoSController/src/main/java/com/agroSoSProyect/Repository/UserRepository.java
@@ -1,13 +1,8 @@
 package com.agroSoSProyect.Repository;
-// Qué hace el jpaRepository
 import com.agroSoSProyect.Models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-// Qué hace el stereotype Repository
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
-
-// Qué hace el @Repository
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
   User findByEmail(String email);
