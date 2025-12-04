a3b0ed8f7e94035c95a05264665093a976d7b672
gabye80 <gabye80@gmail.com>
2025-12-04 19:47:13 +0000

    Otros Models de backend

--- agroSoSController/src/main/java/com/agroSoSProyect/Repository/SensorRespository.java
+++ agroSoSController/src/main/java/com/agroSoSProyect/Repository/SensorRespository.java
@@ -1,12 +1,22 @@
 package com.agroSoSProyect.Repository;
 
-import com.agroSoSProyect.Models.Sensor;
+import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import java.util.List;
+
+import com.agroSoSProyect.Models.Sensor;
 
 @Repository
 public interface SensorRespository extends JpaRepository<Sensor, Long> {
   List<Sensor> findByUser(Long id);
+  //
+  // ------------------------------!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!------------------------------
+  //
+  // Los sensores no se van a encontrar por user, sino al DEVICE al que pertenecen.
+  // Hay que cambiar esto.
+  // Al cambio a lo mejor hay que cambiar cosas dentro de Exception/Sensor
+  //
+  // ------------------------------!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!------------------------------
+  // //
 }
